90e5e788ccf6accce2c1c222f16788552124454d
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-09-01 00:57:03 +0100

    updated compatable versions for IDE's

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.sangeeth"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -32,6 +32,5 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("241.*")
     }
 }
